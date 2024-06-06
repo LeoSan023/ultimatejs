fca4dce59bb5e4a802353780a9fd90d778b80b54
Leonardo Santana <leonardosantana1997@gmail.com>
2024-06-06 14:57:57 -0400

    loop for

---
+++ 03-control-flujo/06-for.js
@@ -0,0 +1,19 @@
+/*
+let i = 2;
+while (i < 2) {
+    if (i%2 == 0){
+        console.log('Numero par', i)
+    }    
+    i++;
+}
+*/
+
+// for(inicializacion;comparacion;terminariteracion){
+for(i = 2; i < 10; i++){
+    if (i % 2 == 0){
+        console.log('Número par:', i);
+    }
+}
+
+
+// console.log('Fuera del while');
