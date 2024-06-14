d0078f07e160cea90d03d00c6821d36acc977fa4
Leonardo Santana <leonardosantana1997@gmail.com>
2024-06-14 14:25:34 -0400

    ejercicio 8

---
+++ 04-ejercicios/08.js
@@ -0,0 +1,27 @@
+/*
+    Crear algoritmo que, dado un array objectos, retorne un array de pares
+*/
+
+let arreglo = [{
+    id : 1,
+    name : 'Nicolas',
+}, {
+    id : 2,
+    name : 'Felipe',
+}, {
+    id : 3,
+    name : 'Chanchito',
+}];
+
+function toPairs(arr) {
+    let pairs = [];
+    for ( idx in arr) {
+        let elemento = arr[idx];
+        pairs[idx] = elemento.id, elemento;
+    }
+
+    return pairs;
+}
+
+let resultado = toPairs(arreglo);
+console.log(resultado);
