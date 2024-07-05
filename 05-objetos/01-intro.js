219c126af0622d7686a5251835e83667edca2087
Leonardo Santana <leonardosantana1997@gmail.com>
2024-07-05 16:13:44 -0400

    objetos e intro

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,12 @@
+let user = {
+    email: 'leonardo@holamundo.io',
+    name: 'Leonardo',
+    direccion : {
+        calle: 'Calle segunda',
+        numero: 44,
+    },
+    activo: true,
+    recuperarClave: function (){
+        console.log('Recuperando clave...');
+    },
+};
